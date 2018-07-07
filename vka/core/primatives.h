07adfd5cf136cbde5b8d97ae6b7056ee6c0f9787
Gavin <gavin.n.lobo@gmail.com>
2018-07-07 11:50:45 -0400

    Plane mesh can now be scaled

--- vka/core/primatives.h
+++ vka/core/primatives.h
@@ -8,7 +8,7 @@ namespace vka
 
 vka::host_mesh box_mesh(float dx , float dy , float dz );
 vka::host_mesh sphere_mesh(float radius , uint32_t rings, uint32_t sectors);
-vka::host_mesh plane_mesh(uint32_t Nx, uint32_t Nz);
+vka::host_mesh plane_mesh(uint32_t Nx, uint32_t Nz, float sx=1, float sz=1);
 
 mesh_t box_mesh_OLD( float dx, float dy, float dz);
 mesh_t sphere_mesh_OLD(float radius , uint32_t rings, uint32_t sectors);
