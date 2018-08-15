ee1ae5d4ebc88c9312cdb944393a64f4f7728340
Gavin <gavin.n.lobo@gmail.com>
2018-08-14 21:04:06 -0400

    Removed all the #if defined USE_REFACTORED from example_01

--- examples/example_01.cpp
+++ examples/example_01.cpp
@@ -48,9 +48,6 @@
 
 
 
-
-#define USE_REFACTORED
-
 /**
  * @brief get_elapsed_time
  * @return
@@ -108,19 +105,10 @@ int main(int argc, char ** argv)
 
     C.create_device(surface); // find the appropriate device
 
-#if defined USE_REFACTORED
+
     vka::Screen Screen(&C);
     Screen.Create(surface, vk::Extent2D(WIDTH,HEIGHT));
-#else
-    // The Screen is essentially a wrapper around the Swapchain, a default Renderpass
-    // and framebuffers.
-    // in VKA we present images to the screen object.
-    // This a simple initialization of creating a screen with depth testing
-    vka::screen * screen = C.new_screen("screen");
-    screen->set_extent( vk::Extent2D(WIDTH,HEIGHT) );
-    screen->set_surface( surface );
-    screen->create();
-#endif
+
 
     //==========================================================================
 
@@ -152,7 +140,7 @@ int main(int argc, char ** argv)
 //==============================================================================
         // Create two buffers, one for vertices and one for indices. THey
         // will each be 1024 bytes long
-#if defined USE_REFACTORED
+
         vka::BufferMemoryPool StagingBufferPool(&C);
         StagingBufferPool.SetMemoryProperties( vk::MemoryPropertyFlagBits::eHostCoherent| vk::MemoryPropertyFlagBits::eHostVisible);
         StagingBufferPool.SetSize(10*1024*1024);
@@ -168,11 +156,7 @@ int main(int argc, char ** argv)
         auto V_buffer = BufferPool.NewSubBuffer(1024);
         auto I_buffer = BufferPool.NewSubBuffer(1024);
         auto U_buffer = BufferPool.NewSubBuffer(1024);
-#else
-        vka::buffer* vertex_buffer = C.new_vertex_buffer("vb", 1024 );
-        vka::buffer* index_buffer  = C.new_index_buffer( "ib", 1024 );
-        vka::buffer* u_buffer      = C.new_uniform_buffer( "ub", 1024);
-#endif
+
 
 
         // This is the vertex structure we are going to use
@@ -184,11 +168,9 @@ int main(int argc, char ** argv)
         };
 
         // allocate a staging buffer of 10MB
-#if defined USE_REFACTORED
+
         auto StagingBuffer  = StagingBufferPool.NewSubBuffer( sizeof(1024*1024*10) );
-#else
-        vka::buffer * staging_buffer = C.new_staging_buffer( "sb", 1024*1024*10 );
-#endif
+
 
         // using the map< > method, we can return an array_view into the
         // memory. We are going to place them in their own scope so that
@@ -204,13 +186,10 @@ int main(int argc, char ** argv)
             vertex[1] = {glm::vec3( 1.0,  0.0, -1.0 ) , glm::vec2(0   , 1) };
             vertex[2] = {glm::vec3(-1.0,  0.0, -1.0 ) , glm::vec2(1   , 1) };
 
-#if defined USE_REFACTORED
+
             vka::MappedMemory m = StagingBuffer->GetMappedMemory();
             m.memcpy( &vertex[0], sizeof(vertex));
-#else
-            void * m = staging_buffer->map_memory();
-            memcpy(m , &vertex[0], sizeof(vertex));
-#endif
+
 
         }
         // Do the same for the index buffer. but we want to specific an
@@ -225,13 +204,10 @@ int main(int argc, char ** argv)
             index[1] = 1;
             index[2] = 2;
 
-#if defined USE_REFACTORED
+
             vka::MappedMemory m  = StagingBuffer->GetMappedMemory() + 3*sizeof(Vertex);
             m.memcpy(&index[0], sizeof(index));
-#else
-            void * m = (uint8_t*)staging_buffer->map_memory() + 3*sizeof(Vertex);
-            memcpy(m , &index[0], sizeof(index));
-#endif
+
 
             LOG << "Index size: " << index.size() << ENDL;
         }
@@ -251,14 +227,9 @@ int main(int argc, char ** argv)
         const vk::DeviceSize index_offset  = vertex_size;
 
 
-#if defined USE_REFACTORED
         copy_cmd.copySubBuffer( StagingBuffer, V_buffer, vk::BufferCopy{vertex_offset, 0 ,vertex_size});
         copy_cmd.copySubBuffer( StagingBuffer, I_buffer, vk::BufferCopy{index_offset,  0 , index_size});
-#else
-        copy_cmd.copyBuffer( *staging_buffer , *vertex_buffer, vk::BufferCopy{ vertex_offset    , 0 , vertex_size } );
-        copy_cmd.copyBuffer( *staging_buffer , *index_buffer , vk::BufferCopy{ index_offset     , 0 , index_size  } );
 
-#endif
 
         copy_cmd.end();
         C.submit_cmd_buffer(copy_cmd);
@@ -269,17 +240,13 @@ int main(int argc, char ** argv)
         // Unmap the memory.
         //   WARNING: DO NOT ACCESS the vertex and index array_views as these
         //            now point to unknown memory spaces
-#if defined USE_REFACTORED
-#else
-        staging_buffer->unmap_memory();
-#endif
 
 //==============================================================================
 // Create a texture
 //
 //==============================================================================
 
-    #if defined USE_REFACTORED
+
         vka::TextureMemoryPool TP(&C);
         TP.SetSize( 50*1024*1024 );
         TP.SetUsage( vk::ImageUsageFlagBits::eColorAttachment
@@ -288,7 +255,7 @@ int main(int argc, char ** argv)
                      | vk::ImageUsageFlagBits::eTransferSrc);
 
 
-    #endif
+
 
     // 1. First load host_image into memory, and specifcy we want 4 channels.
         vka::host_image D("resources/textures/Brick-2852a.jpg",4);
@@ -300,40 +267,22 @@ int main(int argc, char ** argv)
         // 2. Use the context's helper function to create a device local texture
         //    We will be using a texture2d which is a case specific version of the
         //    generic texture
-#if defined USE_REFACTORED
-//        auto Tex = TP.AllocateTexture( vk::Extent3D(D.width(), D.height(),1),
-//                                      1,
-//                                      vk::Format::eR8G8B8A8Unorm,
-//                                      1,
-//                                      vk::ImageTiling::eOptimal,
-//                                      vk::SharingMode::eExclusive);
+
         auto Tex = TP.AllocateTexture2D( vk::Format::eR8G8B8A8Unorm,
                                          vk::Extent2D(D.width(), D.height() ),
                                          1,1
                                          );
-#else
-            vka::texture2d * tex = C.new_texture2d("test_texture");
-            tex->set_size( D.width() , D.height() );
-            tex->set_format(vk::Format::eR8G8B8A8Unorm);
-            tex->set_mipmap_levels(1);
-            tex->create();
-            tex->create_image_view(vk::ImageAspectFlagBits::eColor);
-#endif
+
 
 
     // 3. Map the buffer to memory and copy the image to it.
-#if defined USE_REFACTORED
+
         void * image_buffer_data = StagingBuffer->GetMappedMemory();
-#else
-        void * image_buffer_data = staging_buffer->map_memory();
-#endif
+
 
         memcpy( image_buffer_data, D.data(), D.size() );
 
-#if defined USE_REFACTORED
-#else
-        staging_buffer->unmap_memory();
-#endif
+
 
 
     // 4. Now that the data is on the device. We need to get it from the buffer
@@ -348,13 +297,10 @@ int main(int argc, char ** argv)
         cb1.begin( vk::CommandBufferBeginInfo(vk::CommandBufferUsageFlagBits::eOneTimeSubmit) );
 
             // a. convert the texture to eTransferDstOptimal
-#if defined USE_REFACTORED
+
         cb1.convertTextureLayer( Tex,0,1,vk::ImageLayout::eTransferDstOptimal,
                                  vk::PipelineStageFlagBits::eBottomOfPipe,
                                  vk::PipelineStageFlagBits::eTopOfPipe);
-#else
-        tex->convert_layer(cb1, vk::ImageLayout::eTransferDstOptimal,0,0);
-#endif
 
 
 
@@ -369,22 +315,16 @@ int main(int argc, char ** argv)
                                 .setLayerCount(1) // only copy one layer
                                 .setMipLevel(0);  // only the first mip-map level
 
-#if defined USE_REFACTORED
+
             cb1.copySubBufferToTexture( StagingBuffer, Tex, vk::ImageLayout::eTransferDstOptimal, BIC);
 
-#else
-            tex->copy_buffer( cb1, staging_buffer, BIC);
-#endif
 
 
             // c. convert the texture into eShaderReadOnlyOptimal
-#if defined USE_REFACTORED
+
             cb1.convertTextureLayer( Tex,0,1,vk::ImageLayout::eShaderReadOnlyOptimal,
                                      vk::PipelineStageFlagBits::eBottomOfPipe,
                                      vk::PipelineStageFlagBits::eTopOfPipe);
-#else
-            tex->convert(cb1, vk::ImageLayout::eShaderReadOnlyOptimal);
-#endif
 
 
         // end and submit the command buffer
@@ -399,9 +339,6 @@ int main(int argc, char ** argv)
 // Create a Rendering pipeline
 //
 //==============================================================================
-
-
-#if defined USE_REFACTORED
 
         vka::Pipeline pipeline(&C);
         // Create the graphics Pipeline
@@ -434,54 +371,7 @@ int main(int argc, char ** argv)
                   ->addUniformLayoutBinding(1, 0, vk::ShaderStageFlagBits::eVertex)
                   ->setRenderPass( Screen.GetRenderPass()  )
                   ->create();
-#else
-        // create the vertex shader from a pre compiled SPIR-V file
-        vka::shader* vertex_shader = C.new_shader_module("vs");
-        vertex_shader->load_from_file("resources/shaders/uniform_buffer/uniform_buffer.vert");
 
-        // create the fragment shader from a pre compiled SPIR-V file
-        vka::shader* fragment_shader = C.new_shader_module("fs");
-        fragment_shader->load_from_file("resources/shaders/uniform_buffer/uniform_buffer.frag");
-
-        vka::pipeline* pipeline = C.new_pipeline("triangle");
-
-        // Create the graphics Pipeline
-          pipeline->set_viewport( vk::Viewport( 0, 0, WIDTH, HEIGHT, 0, 1) )
-                  ->setScissor( vk::Rect2D(vk::Offset2D(0,0), vk::Extent2D( WIDTH, HEIGHT ) ) )
-
-                  ->setVertexShader(   vertex_shader )   // the shaders we want to use
-                  ->setFragmentShader( fragment_shader ) // the shaders we want to use
-
-                  // tell the pipeline that attribute 0 contains 3 floats
-                  // and the data starts at offset 0
-                  ->setVertexAttribute(0,0 ,  0,  vk::Format::eR32G32B32Sfloat,  sizeof(Vertex) )
-                  // tell the pipeline that attribute 1 contains 3 floats
-                  // and the data starts at offset 12
-                  ->setVertexAttribute(0,1 , 12,  vk::Format::eR32G32Sfloat,  sizeof(Vertex) )
-
-                  // Triangle vertices are drawn in a counter clockwise manner
-                  // using the right hand rule which indicates which face is the
-                  // front
-                  ->setFrontFace(vk::FrontFace::eCounterClockwise)
-
-                  // Cull all back facing triangles.
-                  ->setCullMode(vk::CullModeFlagBits::eBack)
-
-                  // Tell the shader that we are going to use a texture
-                  // in Set #0 binding #0
-                  ->addTextureLayoutBinding(0, 0, vk::ShaderStageFlagBits::eFragment)
-
-                  // Tell teh shader that we are going to use a uniform buffer
-                  // in Set #0 binding #0
-                  ->addUniformLayoutBinding(1, 0, vk::ShaderStageFlagBits::eVertex)
-                  //
-        #if defined USE_REFACTORED
-                  ->setRenderPass( Screen.GetRenderPass()  )
-        #else
-                  ->set_render_pass( screen->get_renderpass() )
-        #endif
-                  ->create();
-#endif
 
 
 //==============================================================================
@@ -492,23 +382,16 @@ int main(int argc, char ** argv)
 //==============================================================================
     // we want a descriptor set for set #0 in the pipeline.
     //  attach our texture to binding 0 in the set.
-#if defined USE_REFACTORED
+
     vka::DescriptorSet_p  texture_descriptor = pipeline.createNewDescriptorSet(0, &descriptor_pool);
     texture_descriptor->AttachSampler(0, Tex);
-#else
-    vka::DescriptorSet_p  texture_descriptor = pipeline.createNewDescriptorSet(0, &descriptor_pool);
-    texture_descriptor->attach_sampler(0, tex);
-#endif
+
     texture_descriptor->update();
 
 
-#if defined USE_REFACTORED
     vka::DescriptorSet_p  ubuffer_descriptor = pipeline.createNewDescriptorSet(1, &descriptor_pool);
     ubuffer_descriptor->AttachUniformBuffer(0,U_buffer, 10);
-#else
-    vka::DescriptorSet_p  ubuffer_descriptor = pipeline.createNewDescriptorSet(1, &descriptor_pool);
-    ubuffer_descriptor->attach_uniform_buffer(0, u_buffer, 10, 0);
-#endif
+
     ubuffer_descriptor->update();
 
     // This is the structure of the uniform buffer we want.
@@ -521,11 +404,8 @@ int main(int argc, char ** argv)
     };
 
 
-#if defined USE_REFACTORED
+
     vka::MappedMemory StagingBufferMap   = StagingBuffer->GetMappedMemory();
-#else
-    vka::array_view<uniform_buffer_t> StagingBufferMap = staging_buffer->map<uniform_buffer_t>();
-#endif
 
     vka::command_buffer cb = cp->AllocateCommandBuffer();
 
@@ -571,23 +451,17 @@ int main(int argc, char ** argv)
 
 
       // Copy the uniform buffer data from the stating buffer to the uniform buffer
-#if defined USE_REFACTORED
+
       cb.copySubBuffer( StagingBuffer,     U_buffer , vk::BufferCopy{ 0,0,sizeof(uniform_buffer_t) } );
-#else
-      cb.copyBuffer( *staging_buffer , *u_buffer , vk::BufferCopy{ 0,0,sizeof(uniform_buffer_t) } );
-#endif
 
-#if defined USE_REFACTORED
+
+
       uint32_t frame_index = Screen.GetNextFrameIndex(image_available_semaphore);
-#else
-      uint32_t frame_index = screen->prepare_next_frame(image_available_semaphore);
-#endif
 
-#if defined USE_REFACTORED
+
+
       cb.beginRender(Screen, frame_index);
-#else
-      screen->beginRender(cb, frame_index);
-#endif
+
 
       // bind the pipeline that we want to use next
             cb.bindPipeline( vk::PipelineBindPoint::eGraphics, pipeline );
@@ -605,23 +479,16 @@ int main(int argc, char ** argv)
                                                     nullptr );
 
      // bind the vertex/index buffers
-#if defined USE_REFACTORED
       cb.bindVertexSubBuffer(0, V_buffer );// ( m_VertexBuffer, 0);
       cb.bindIndexSubBuffer( I_buffer, vk::IndexType::eUint16);
-#else
-      cb.bindVertexBuffers(0, vertex_buffer->get(), {0} );// ( m_VertexBuffer, 0);
-      cb.bindIndexBuffer(  index_buffer->get() , 0 , vk::IndexType::eUint16);
-#endif
+
 
 
     // draw 3 indices, 1 time, starting from index 0, using a vertex offset of 0
             cb.drawIndexed(3, 1, 0 , 0, 0);
 
-#if defined USE_REFACTORED
             cb.endRenderPass();
-#else
-            screen->endRender(cb);
-#endif
+
       cb.end();
 
       // Submit the command buffers, but wait until the image_available_semaphore
@@ -630,11 +497,8 @@ int main(int argc, char ** argv)
 
       // present the image to the surface, but wait for the render_complete_semaphore
       // to be flagged by the submit_command_buffer
-#if defined USE_REFACTORED
       Screen.PresentFrame(frame_index, render_complete_semaphore);
-#else
-      screen->present_frame( frame_index, render_complete_semaphore);
-#endif
+
 
       std::this_thread::sleep_for( std::chrono::milliseconds(3) );
     }
