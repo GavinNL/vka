313e65bc105dcb56b28530492a7dd808da6dfef1
Gavin <gavin.n.lobo@gmail.com>
2018-08-20 20:47:58 -0400

    Added vka::CommandPool

---
+++ src/core2/CommandPool.cpp
@@ -0,0 +1,45 @@
+#include <vka/core2/CommandPool.h>
+#include <vka/core/context.h>
+
+namespace vka
+{
+
+void vka::CommandPool::create()
+{
+    vk::CommandPoolCreateInfo poolInfo;
+
+    poolInfo.queueFamilyIndex = get_parent_context()->get_queue_family().graphics;
+    poolInfo.flags            = vk::CommandPoolCreateFlagBits::eResetCommandBuffer; // Optional
+
+    m_command_pool = get_device().createCommandPool(poolInfo);
+
+    if( !m_command_pool )
+    {
+        throw std::runtime_error("Failed to create command pool!");
+    }
+    LOG << "Command Pool created" << ENDL;
+}
+
+command_buffer CommandPool::AllocateCommandBuffer(vk::CommandBufferLevel level)
+{
+    vk::CommandBufferAllocateInfo allocInfo;
+
+    allocInfo.level              = level;
+    allocInfo.commandPool        = m_command_pool;
+    allocInfo.commandBufferCount = 1;
+
+    std::vector<vk::CommandBuffer> commandBuffer_v = get_device().allocateCommandBuffers( allocInfo );
+
+    if( commandBuffer_v.size() == 0 )
+        throw std::runtime_error("Error creating command buffers");
+
+
+    return commandBuffer_v[0];
+}
+
+void CommandPool::FreeCommandBuffer(vka::command_buffer cmd)
+{
+     get_device().freeCommandBuffers( m_command_pool, 1, &cmd);
+}
+
+}
---
+++ vka/core2/CommandPool.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef VKA_COMMANDPOOL2_H
+#define VKA_COMMANDPOOL2_H
+
+#include <vka/core/log.h>
+#include <vka/core/command_buffer.h>
+#include <vka/core/context_child.h>
+
+namespace vka
+{
+
+class CommandPool : public context_child
+{
+public:
+    CommandPool(context * parent) : context_child(parent)
+    {
+    }
+
+    ~CommandPool()
+    {
+        if(m_command_pool)
+        {
+            get_device().destroyCommandPool(m_command_pool);
+            m_command_pool = vk::CommandPool();
+        }
+    }
+
+    void create();
+
+    void FreeCommandBuffer(vka::command_buffer cmd);
+
+    vka::command_buffer AllocateCommandBuffer(vk::CommandBufferLevel level = vk::CommandBufferLevel::ePrimary );
+protected:
+    vk::CommandPool m_command_pool;
+};
+
+}
+
+#endif
