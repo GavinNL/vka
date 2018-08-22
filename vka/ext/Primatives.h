5103dbca696e097c0936b37e1186c12824cfe17d
Gavin <gavin.n.lobo@gmail.com>
2018-08-22 18:12:47 -0400

    Removed some old functions

--- vka/ext/Primatives.h
+++ vka/ext/Primatives.h
@@ -10,8 +10,6 @@ vka::host_mesh box_mesh(float dx , float dy , float dz );
 vka::host_mesh sphere_mesh(float radius , uint32_t rings, uint32_t sectors);
 vka::host_mesh plane_mesh(uint32_t Nx, uint32_t Nz, float sx=1, float sz=1);
 
-mesh_t box_mesh_OLD( float dx, float dy, float dz);
-mesh_t sphere_mesh_OLD(float radius , uint32_t rings, uint32_t sectors);
-mesh_t plane_mesh_OLD(uint32_t Nx, uint32_t Nz);
+
 }
 #endif
